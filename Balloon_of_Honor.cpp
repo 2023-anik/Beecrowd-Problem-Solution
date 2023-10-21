337d4a1adb357e0d3ddef380be98bf768ba230c3
Anik Sen <cse2023anik@gmail.com>
2023-10-22 03:11:27 +0600

    2987-Balloon_of_Honor

---
+++ Balloon_of_Honor.cpp
@@ -0,0 +1,10 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    char c;
+    cin>>c;
+    cout<<c-64<<endl;//to find uppercase letter's position
+    //cout<<c-96<<endl;//to find lowercase letter's position
+    return 0;
+}
